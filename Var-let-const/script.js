f39bb2ef1b2504101ad4a695c701417f6df9a51e
Your Name <kaifali.web@gmail.com>
2025-11-22 19:05:19 +0530

    Initial commit: JavaScript learning files

---
+++ Var-let-const/script.js
@@ -0,0 +1,49 @@
+var v= "Kaif";
+// console.log(v);
+
+// v="Kaif Ali";
+// console.log(v);
+
+// var v= "Atif";
+// console.log(v);
+
+// {
+//     // console.log(v);
+//     var s="kashif";
+// }
+// console.log(s);
+
+
+
+let l= "Kaif";
+// console.log(l);
+
+// l="Kaif Ali";
+// console.log(l);
+
+// let l= "Atif";
+// console.log(l);
+
+
+// {
+//     // console.log(l);
+//     let a="kashif";
+// }
+// console.log(a);
+
+
+const c= "Kaif";
+// console.log(c);
+
+// c="Kaif Ali";
+// console.log(c);
+
+// const c= "Atif";
+// console.log(c);
+
+
+// {
+//     // console.log(c);
+//     const b="kashif";
+// }
+// console.log(b);
